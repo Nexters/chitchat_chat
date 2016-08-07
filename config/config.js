993308d190b1de297bf6308b4d0a012a0e57e694
Taeyoung Kim <Taeyoung Kim>
2016-08-07 23:04:29 +0900

    init chat server

---
+++ config/config.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var path = require('path');
+var _ = require('lodash');
+
+let nodeEnv = process.env.NODE_ENV || 'development';
+
+let config = {
+  db: '',
+  chat: '',
+};
+
+config = _.mapValues(config, function (val, configName) {
+  var configPath = path.resolve(__dirname, nodeEnv, configName + '.js');
+  return require(configPath);
+});
+
+module.exports = config;
